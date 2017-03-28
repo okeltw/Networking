87dc78b3c3220fca8991c7934eea7c78a3658673
okeltw <okeltw@mail.uc.edu>
2017-03-28 13:25:07 -0400

    Finished server code

--- Project2/server.py
+++ Project2/server.py
@@ -1,12 +1,24 @@
-from socket import * 
-serverSocket = socket(AF_INET, SOCK_STREAM) 
+#from socket import * 
+import socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
 #Prepare a sever socket 
-servHost = serverSocket.gethostname()
+servHost = '127.0.0.1' # socket.gethostname()
 servPort = 80
 serverSocket.bind((servHost, servPort))
 
+print 'Binding to host', servHost, 'on port', servPort
+
 packetSize = 2048
+
+header = "Content-Type: text/html\r\n"
+header += "Connection: close\r\n" 
+header += "\r\n"
+headerOK = "HTTP/1.1 200 OK\r\n" + header
+headerNF = "HTTP/1.1 404 Not Found\r\n" + header
+           
+textNF = "<html><body>404 ERROR: File Not Found</body></html>"
+
 
 while True: 
     #Establish the connection 
@@ -18,14 +30,17 @@ while True:
         filename = message.split()[1] 
         f = open(filename[1:]) 
         outputdata = f.read() 
+        print 'Filename: ', filename
+        print 'OutputData: ', outputdata
         #Send one HTTP header line into socket 
-        connectionSocket.send('HTTP/1.1 200 OK')
-        for i in range(0, len(outputdata)): 
-            connectionSocket.send(outputdata[i]) 
+        connectionSocket.send(headerOK)
+
+        #for i in range(0, len(outputdata)): 
+        connectionSocket.send(outputdata) 
         connectionSocket.close() 
     except IOError: 
         #Send response message for file not found 
-        connectionSocket.send('HTTP/1.1 404 Not Found')
+        connectionSocket.send(headerNF)
+        connectionSocket.send(textNF)
         #Close client socket 
         connectionSocket.close()
-
